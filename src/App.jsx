a7734f8ce5e7e2da2e33bad267e04d9e280f3eb7
Ehtesham Ahmed <ahmedehtesham609@gmail.com>
2023-10-25 17:11:54 +0530

    header component , route, css design

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-
+import Header from './components/header';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/SignIn" element={<SignIn />} />
-      <Route path="/SignUp" element={<SignUp />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes> 
+      <Header />  
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/About' element={<About />} />
+          <Route path='/SignIn' element={<SignIn />} />
+          <Route path='/SignUp' element={<SignUp />} />
+          <Route path='/Profile' element={<Profile />} />
+        </Routes> 
     </BrowserRouter>
   )
 }
