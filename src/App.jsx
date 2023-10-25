5627f081d2e6d99da338025fe86563997a85fa3d
Ehtesham Ahmed <31020489+ehtu609@users.noreply.github.com>
2023-10-25 22:00:13 +0530

    mongodb database schema and rules

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
-import Header from './components/header';
+import Header from './components/Header';
 
 export default function App() {
   return (
